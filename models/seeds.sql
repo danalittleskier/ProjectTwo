c1e7ba597107b75d80882c1b6b5bf92edeb436bf
tbsteeze <trevbrow@live.com>
2019-10-19 12:58:21 -0600

    added login modal

---
+++ models/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO Tool_Table (tool_name, tool_category, tool_price)
+    VALUES ("Circular Saw", "Power Tools", 100),
+    VALUES ("Circular Saw", "Power Tools", 100),
+    VALUES ("Claw Hammer", "Hand Tools", 100);
+
