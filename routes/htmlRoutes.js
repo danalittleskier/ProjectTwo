43d89a49bfb8929550102a59fb6110d99cdfea90
DanaLittleSkier <dana.alexandrescu@gmail.com>
2019-10-18 19:51:16 -0600

    More travis complain fixes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -13,9 +13,19 @@ module.exports = function(app) {
 
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+    db.Example.findOne({ where: { id: req.params.id } }).then(function(
+      dbExample
+    ) {
       res.render("example", {
         example: dbExample
+      });
+    });
+  });
+
+  app.get("/users", function(req, res) {
+    db.User.findAll({}).then(function(dbUsers) {
+      res.render("users", {
+        users: dbUsers
       });
     });
   });
