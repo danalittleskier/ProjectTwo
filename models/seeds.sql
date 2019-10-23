5ecff5ff12917269fd5402ee187cde4190c91c5b
tbsteeze <trevbrow@live.com>
2019-10-23 13:24:34 -0600

    changed rented to boolean in seeds.sql

--- models/seeds.sql
+++ models/seeds.sql
@@ -10,28 +10,28 @@ INSERT INTO members (first_name, last_name , address, username, createdAt, updat
 VALUES ("Jim", "Builder", "1 Victory St", "jimbuilder", now(), now());
 
 INSERT INTO tools (name, description, category, price, rented, renter, createdAt, updatedAt, MemberId)
-VALUES ("Chainsaw", "A cool description", "Power Tools", 25.00, "N", null,now(), now(), 1);
+VALUES ("Chainsaw", "A cool description", "Power Tools", 25.00, true, null,now(), now(), 1);
 
 INSERT INTO tools (name, description, category, price, rented, renter, createdAt, updatedAt, MemberId)
-VALUES ("Belt Sander", "A device larger than most sanders out there", "Power Tools", 25.00, "N", null, now(), now(), 1);
+VALUES ("Belt Sander", "A device larger than most sanders out there", "Power Tools", 25.00, false, null, now(), now(), 1);
 
 INSERT INTO tools (name, description, category, price, rented, renter, createdAt, updatedAt, MemberId)
-VALUES ("Tablesaw", "Dont cut your fingers off", "Power Tools", 25.00, "N", null,now(), now(), 1);
+VALUES ("Tablesaw", "Dont cut your fingers off", "Power Tools", 25.00, true, null,now(), now(), 1);
 
 INSERT INTO tools (name, description, category, price, rented, renter, createdAt, updatedAt, MemberId)
-VALUES ("Weed Whacker", "Spin twine really, really fast", "Garden Tools", 25.00, "N", null, now(), now(), 1);
+VALUES ("Weed Whacker", "Spin twine really, really fast", "Garden Tools", 25.00, false, null, now(), now(), 1);
 
 INSERT INTO tools (name, description, category, price, rented, renter, createdAt, updatedAt, MemberId)
-VALUES ("3D Printer", "Create literally anything out of thin air and a little bit of plastic", "Machine Tools", 25.00, "N", null,now(), now(), 1);
+VALUES ("3D Printer", "Create literally anything out of thin air and a little bit of plastic", "Machine Tools", 25.00, false, null,now(), now(), 1);
 
 INSERT INTO tools (name, description, category, price, rented, renter, createdAt, updatedAt, MemberId)
-VALUES ("Hammer", "Nail Stuff", "Power Tools", 25.00, "N", null, now(), now(), 1);
+VALUES ("Hammer", "Nail Stuff", "Power Tools", 25.00, false, null, now(), now(), 1);
 
 INSERT INTO tools (name, description, category, price, rented, renter, createdAt, updatedAt, MemberId)
-VALUES ("Screw Driver", "Screw er, the old fashioned way", "Hand Tools", 25.00, "N", null,now(), now(), 1);
+VALUES ("Screw Driver", "Screw er, the old fashioned way", "Hand Tools", 25.00, false, null,now(), now(), 1);
 
 INSERT INTO tools (name, description, category, price, rented, renter, createdAt, updatedAt, MemberId)
-VALUES ("Measuring Tape", "Size Does Matter", "Hand Tools", 25.00, "N", null, now(), now(), 1);
+VALUES ("Measuring Tape", "Size Does Matter", "Hand Tools", 25.00, false, null, now(), now(), 1);
 
 
 
