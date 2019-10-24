73c3adc0816eef0da0d753b62f4a71dbe7cd509f
Guillermo Villalta <mexcelus@gmail.com>
2019-10-24 11:02:42 -0600

    member gets created when user gets signed up

--- public/js/signup.js
+++ public/js/signup.js
@@ -3,30 +3,61 @@ $(document).ready(function() {
   var signUpForm = $("form.signup");
   var emailInput = $("input#email-input");
   var passwordInput = $("input#password-input");
+  var firstNameInput = $("input#first-name-input");
+  var lastNameInput = $("input#last-name-input");
+  var zipInput = $("input#zip-input");
+  var usernameInput = $("input#username-input");
 
   // When the signup button is clicked, we validate the email and password are not blank
   signUpForm.on("submit", function(event) {
     event.preventDefault();
     var userData = {
       email: emailInput.val().trim(),
-      password: passwordInput.val().trim()
+      password: passwordInput.val().trim(),
+      firstName: firstNameInput.val().trim(),
+      lastName: lastNameInput.val().trim(),
+      zip: zipInput.val().trim(),
+      username: usernameInput.val().trim()
     };
+
+    console.log(userData.email, userData.username, userData.zip);
 
     if (!userData.email || !userData.password) {
       return;
     }
     // If we have an email and password, run the signUpUser function
-    signUpUser(userData.email, userData.password);
+    signUpUser(
+      userData.email,
+      userData.password,
+      userData.firstName,
+      userData.lastName,
+      userData.zip,
+      userData.username
+    );
+    // createMember(
+    //   userData.firstName,
+    //   userDate.lastName,
+    //   userData.zip,
+    //   userData.username
+    // );
     emailInput.val("");
     passwordInput.val("");
+    firstNameInput.val("");
+    lastNameInput.val("");
+    zipInput.val("");
+    usernameInput.val("");
   });
 
   // Does a post to the signup route. If successful, we are redirected to the members page
   // Otherwise we log any errors
-  function signUpUser(email, password) {
+  function signUpUser(email, password, firstName, lastName, zip, username) {
     $.post("/api/signup", {
       email: email,
-      password: password
+      password: password,
+      firstName: firstName,
+      lastName: lastName,
+      zip: zip,
+      username: username
     })
       // eslint-disable-next-line no-unused-vars
       .then(function(data) {
